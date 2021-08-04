b86d4420200f0cc603ad75119b169e3ed0640fdb
adarsha <adarshkannada@gmail.com>
2021-08-05 00:35:04 +0530

    added test util import data method

--- src/main/java/com/goibibo/qa/utilties/TestUtil.java
+++ src/main/java/com/goibibo/qa/utilties/TestUtil.java
@@ -1,10 +1,17 @@
 package com.goibibo.qa.utilties;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import com.goibibo.qa.base.TestBase;
 
@@ -17,6 +24,10 @@ public class TestUtil extends TestBase{
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 10;
 	
+	static Workbook book;
+	static Sheet sheet;
+	public static String TESTDATA_SHEET_PATH = "/home/freedom/Documents/eclipse/TestAutomation/src/main/java/com/goibibo/qa/testData/testData.xlsx";
+	
 	public void scrollPageDown(WebDriver driver){
 		JavascriptExecutor js = ((JavascriptExecutor) driver);
 		js.executeScript("window.scrollTo(0,document.body.scrollHeight)"); 
@@ -27,5 +38,31 @@ public class TestUtil extends TestBase{
 		js.executeScript("arguments[0].scrollIntoView(true);", element);
 		
 	}
+	
+	public static Object[][] getTestData(String sheetName) throws InvalidFormatException {
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(TESTDATA_SHEET_PATH);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		// System.out.println(sheet.getLastRowNum() + "--------" +
+		// sheet.getRow(0).getLastCellNum());
+		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+				// System.out.println(data[i][k]);
+			}
+		}
+		return data;
+	}
+	
 
 }
