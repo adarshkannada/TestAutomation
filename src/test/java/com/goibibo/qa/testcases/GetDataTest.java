546383cd0aec759880140641d07f95a59292061c
adarsha <adarshkannada@gmail.com>
2021-08-07 00:39:59 +0530

    getDataTest OS compatibility

--- src/test/java/com/goibibo/qa/testcases/GetDataTest.java
+++ src/test/java/com/goibibo/qa/testcases/GetDataTest.java
@@ -1,5 +1,8 @@
 package com.goibibo.qa.testcases;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.testng.annotations.Test;
 
 import com.goibibo.qa.utilties.XLS_Reader;
@@ -12,7 +15,16 @@ public class GetDataTest {
 	 public void GetDataFromExcel()
 	 {
 		 Object data[][] = null;
-		 xlreader = new XLS_Reader("C:\\Users\\HP\\eclipse-workspace\\TestAutomation\\src\\main\\java\\com\\goibibo\\qa\\testData\\testData.xlsx");
+//		 xlreader = new XLS_Reader("/home/freedom/Documents/eclipse/TestAutomation/src/main/java/com/goibibo/qa/testData/testData.xlsx");
+		 
+		 if(System.getProperty("os.name").equals("Linux")){
+			xlreader = new XLS_Reader("/home/freedom/Documents/eclipse/TestAutomation/src/main/java/com/goibibo/qa/testData/testData.xlsx");
+		}
+		else if(System.getProperty("os.name").equals("Windows 10")){
+				xlreader = new XLS_Reader("C:\\Users\\HP\\eclipse-workspace\\TestAutomation\\src\\main\\java\\com\\goibibo\\qa\\testData\\testData.xlsx");
+			}
+		 
+		 
 		 data =  xlreader.getCellData("Sheet1");
 		 for(int i =1;i<=2;i++)
 		 {
